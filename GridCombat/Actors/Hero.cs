22de28a973a4320c59fc19a907b5547a261f39dc
TRushton95 <tom.rushton@hotmail.co.uk>
2017-11-13 19:43:34 +0000

    Added Hero class

---
+++ GridCombat/Actors/Hero.cs
@@ -0,0 +1,58 @@
+﻿namespace GridCombat.Actors
+{
+    #region Usings
+
+    using Microsoft.Xna.Framework.Graphics;
+
+    #endregion
+
+    class Hero : BaseActor
+    {
+        #region Fields
+
+        private static float diameter = 50;
+
+        #endregion
+
+        #region Constructors
+
+        public Hero(int maxHealth, int maxEnergy, float posX, float posY, Texture2D texture)
+            : base(diameter, diameter, posX, posY, texture)
+        {
+            this.MaxHealth = maxHealth;
+            this.CurrentHealth = maxHealth;
+            this.MaxEnergy = maxEnergy;
+            this.CurrentEnergy = maxEnergy;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int MaxHealth
+        {
+            get;
+            set;
+        }
+
+        public int MaxEnergy
+        {
+            get;
+            set;
+        }
+
+        public int CurrentHealth
+        {
+            get;
+            set;
+        }
+
+        public int CurrentEnergy
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
