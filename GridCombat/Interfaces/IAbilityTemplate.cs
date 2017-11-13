0071954b8bb17e8f021c8ba54935ec429d450681
TRushton95 <tom.rushton@hotmail.co.uk>
2017-11-13 22:59:53 +0000

    Added interface for ability templates and target validators

---
+++ GridCombat/Interfaces/IAbilityTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace GridCombat.Interfaces.Effects
+{
+    #region Usings
+
+    using GridCombat.Actors;
+    using System.Collections.Generic;
+
+    #endregion
+
+    interface IAbilityTemplate
+    {
+        void GetAffectedTiles(List<List<Tile>> tiles);
+    }
+}
---
+++ GridCombat/Interfaces/ITargetValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace GridCombat.Interfaces
+{
+    #region usings
+
+    using GridCombat.Actors;
+
+    #endregion
+
+    interface ITargetValidator
+    {
+        bool Validate(Tile tile);
+    }
+}
