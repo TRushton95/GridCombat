c91e6f19fcf8dd4b0e9bb4f5a893b9d0b652f4e7
TRushton95 <tom.rushton@hotmail.co.uk>
2017-11-23 23:09:29 +0000

    Added GameState class

---
+++ GridCombat/GameState.cs
@@ -0,0 +1,30 @@
+﻿namespace GridCombat
+{
+    class GameState : BaseInstance
+    {
+        #region Properties
+
+        public int CurrentPlayer
+        {
+            get;
+            set;
+        }
+
+        public int Turn
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void EndTurn()
+        {
+
+        }
+
+        #endregion
+    }
+}
