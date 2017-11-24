6ca87376f09fb001a23a6c2a3574cb4bdd117ee5
TRushton95 <tom.rushton@hotmail.co.uk>
2017-11-24 18:25:16 +0000

    Ability inherits BaseInstance and begun refactoring execute method

--- GridCombat/Abilities/Ability.cs
+++ GridCombat/Abilities/Ability.cs
@@ -1,17 +1,17 @@
 ﻿namespace GridCombat.Abilities
 {
     #region Usings
-
-    using System;
+    
     using System.Collections.Generic;
     using GridCombat.Actors;
     using GridCombat.Interfaces;
     using GridCombat.Enums;
     using GridCombat.Services;
+    using System;
 
     #endregion
 
-    class Ability
+    class Ability : BaseInstance
     {
         #region Constructors
 
@@ -58,6 +58,23 @@
         public bool ValidateTarget(Tile targetTile)
         {
             return TargetValidatorService.Validate(this.Caster, targetTile, this.TargetType);
+        }
+
+        public void Execute(Tile targetTile)
+        {
+            if (!ValidateTarget(targetTile))
+            {
+                Console.WriteLine("Cannot execute ability on that target");
+                return;
+            }
+
+            //Get affected tiles
+            //Repeat effect execution for each affect tile
+
+            foreach (IEffect effect in Effects)
+            {
+                effect.Execute(Caster, targetTile);
+            }
         }
 
         #endregion
